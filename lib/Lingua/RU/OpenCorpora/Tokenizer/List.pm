289efaa37ec938079213ed21ffe3cdee14e2b71a
Alexey Surikov <ksurent@gmail.com>
2011-11-20 16:52:52 +0100

    added update API removed backward compatibility

--- lib/Lingua/RU/OpenCorpora/Tokenizer/List.pm
+++ lib/Lingua/RU/OpenCorpora/Tokenizer/List.pm
@@ -8,8 +8,11 @@ our $VERSION = 0.03;
 use IO::File;
 use File::Spec;
 use Carp qw(croak);
+use Encode qw(decode);
 use IO::Uncompress::Gunzip;
 use File::ShareDir qw(dist_dir);
+
+sub data_version { 0.03 }
 
 sub new {
     my($class, $name, $args) = @_;
@@ -18,6 +21,7 @@ sub new {
 
     $args             ||= {};
     $args->{data_dir} ||= dist_dir('Lingua-RU-OpenCorpora-Tokenizer');
+    $args->{root_url} ||= 'http://opencorpora.org/files/export/tokenizer_data';
 
     my $self = bless {
         %$args,
@@ -38,27 +42,31 @@ sub in_list {
 sub _load {
     my $self = shift;
 
-    my($class, $error, @args);
-
     my $fn = $self->_path;
-    if($fn =~ /\.gz$/) {
-        $class = 'IO::Uncompress::Gunzip';
-        $error = \$IO::Uncompress::Gunzip::GunzipError;
-        @args  = ();
-    }
-    else {
-        $class = 'IO::File';
-        $error = \$!;
-        @args  = ('<:utf8');
-    }
+    my $fh = IO::Uncompress::Gunzip->new($fn) or die "$fn: $IO::Uncompress::Gunzip::GunzipError";
 
-    my $fh = $class->new($fn, @args) or die "$fn: $$error";
     chomp($self->{version} = $fh->getline);
-    $self->_parse_list([$fh->getlines]);
+
+    my @data = map decode('utf-8', $_), $fh->getlines;
+    $self->_parse_list(\@data);
+
     $fh->close;
 
     return;
 }
+
+sub _update {
+    my($self, $new_data) = @_;
+
+    my $fn = $self->_path;
+    my $fh = IO::File->new($fn, '>') or croak "$fn: $!";
+    $fh->binmode;
+    $fh->print($new_data);
+    $fh->close;
+
+    $self->_load;
+}
+
 
 sub _parse_list {
     my($self, $list) = @_;
@@ -72,13 +80,23 @@ sub _parse_list {
 sub _path {
     my $self = shift;
 
-    for('.gz', '') { # backwards compatibility
-        my $fn = File::Spec->catfile($self->{data_dir}, $self->{name} . $_);
+    File::Spec->catfile($self->{data_dir}, "$self->{name}.gz");
+}
 
-        return $fn if -e $fn;
+sub _url {
+    my($self, $mode) = @_;
+
+    $mode ||= 'file';
+
+    my $url = join '/', $self->{root_url}, $self->data_version, $self->{name};
+    if($mode eq 'file') {
+        $url .= '.gz';
+    }
+    elsif($mode eq 'version') {
+        $url .= '.latest';
     }
 
-    Carp::croak "Couldn't find file for '$self->{name}'";
+    $url;
 }
 
 1;
